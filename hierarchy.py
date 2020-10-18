13bc55c080b9fa8643b66e2b25d112a9a667b65b
Yinfei Wang <36552067+yinfeiw1998@users.noreply.github.com>
2020-10-18 03:08:58 -0700

    hierarchy.py file finished

--- hierarchy.py
+++ hierarchy.py
@@ -1,51 +1,53 @@
+import inspect
+
 class Recorder(object):
-    def record(self, skill_name, skill_arg, skill_cnt, ret_name, ret_val, sub_name, sub_arg):
-        # TODO: append to trace
-        pass
+    def __init__(self):
+        self.trace = []
+
+    def record(self, **kwargs):
+        self.trace.append(kwargs)
 
 
 class Skill(object):
-    def __init__(self, f):
-        self.name = f.__name__
-        self.f = f
+    def __init__(self, skill_func):
+        self.name = skill_func.__name__
+        self.skill_func = skill_func
 
     def __call__(self, *args):
-        cnt = 0
-        for caller in call_stack:
-            if isinstance(caller, Skill):
-                self.recorder = caller.recorder
-                self.recorder.record(
-                    skill_name=caller.name,
-                    skill_arg=caller.args,
-                    skill_cnt=caller.cnt,
-                    ret_name=caller.ret_name,
-                    ret_val=caller.ret_val,
-                    sub_name=self.name,
-                    sub_arg=args)
-                caller.cnt += 1
+        cnt = [0]
+        ret_name = [None]
+        ret_val = [None]
+
+        for caller in inspect.stack()[1:]:
+            caller_locals = caller[0].f_locals
+
+            if ('self' in caller_locals) and isinstance(caller_locals['self'], Skill):
+                recorder = caller_locals['recorder']
+                recorder.record(
+                    skill_name= caller_locals['self'].name,
+                    skill_arg= caller_locals['args'],
+                    skill_cnt= caller_locals['cnt'][0],
+                    ret_name= caller_locals['ret_name'][0],
+                    ret_val= caller_locals['ret_val'][0],
+                    sub_name= self.name,
+                    sub_arg= args)
+
+                caller_locals['cnt'][0] += 1
                 break
         else:
-            # root skill
-            caller = None
-            self.recorder = Recorder()
-        rvals = self.f(*args)
-        if caller is not None:
-            caller.ret_name = self.name
-            caller.ret_val = rvals
+            # root skill 
+            caller_locals = None
+            recorder = Recorder()
+
+        rvals = self.skill_func(*args)
+
+        if caller_locals is not None: 
+
+            caller[0].f_locals['ret_name'][0] = self.name
+            caller[0].f_locals['ret_val'][0] = rvals
         else:
+            print(recorder.trace)
             # TODO: dump recorder
-            pass
         return rvals
 
 
-class Action(Skill):
-    pass
-
-# def skill(f):
-#     @functools.wraps(f)
-#     def wrapper(agent, *args):
-#         rvals = f(*args)
-#         agent.record(f, args, rvals)
-#         return rvals
-#
-#     return wrapper
